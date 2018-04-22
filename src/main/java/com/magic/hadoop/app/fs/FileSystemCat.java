36dea6de15e5dfb0dfb7b1ce520f14dc5f36a56a
Magicfeng007 <xzf1108@foxmail.com>
2018-04-22 17:32:22 +0800

    将FSDataInputStream改为InputStream

--- src/main/java/com/magic/hadoop/app/fs/FileSystemCat.java
+++ src/main/java/com/magic/hadoop/app/fs/FileSystemCat.java
@@ -1,7 +1,6 @@
 package com.magic.hadoop.app.fs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
